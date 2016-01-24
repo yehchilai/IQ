3b2b0aa2926db5abff9a727e83bba234d74151b6
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-01-24 14:58:47 -0800

    231. Power of Two (Easy)

---
+++ 231 Power of Two (Easy).java
@@ -0,0 +1,22 @@
+/*
+This question is from https://leetcode.com/problems/power-of-two/
+
+Given an integer, write a function to determine if it is a power of two.
+
+
+Time Complexity:O(N)
+*/
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1) return true;
+        if(n%2 != 0 || n < 1) return false;
+        return isPowerOfTwo(n/2);
+    }
+}
+
+// Answer II : Time Complexity: O(1)
+public class Solution {
+	public boolean isPowerOfTwo(int n) { //if n is power  of  2 ,n just has one bit is 1
+	    return n>0 && (n&(n-1))==0;
+	}
+}
