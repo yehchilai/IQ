f07e39f9f15a1162623259a03f64120967e2a6e9
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-01-30 18:21:28 -0800

    107. Binary Tree Level Order Traversal II - incomplete

--- Easy/198 House Robber.java
+++ Easy/198 House Robber.java
@@ -10,10 +10,19 @@ Given a list of non-negative integers representing the amount of money of each h
 determine the maximum amount of money you can rob tonight "without alerting the police".
 
 
-Time Complexity: O()
+Time Complexity: O(N) , N: the number of houses 
 */
 public class Solution {
     public int rob(int[] nums) {
-        
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        int secondPrevious = nums[0], previous = Math.max(nums[0], nums[1]), sum = previous;
+        for(int i = 2; i < nums.length; i++){
+        	sum = Math.max(previous, secondPrevious + nums[i]);
+        	secondPrevious  = previous;
+        	previous = sum;
+        }
+        return sum;
     }
 }
