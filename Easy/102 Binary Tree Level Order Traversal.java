bf8dac1f57758de910723b45ec542a07f0d02ce1
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-02-05 14:54:36 -0800

    102. Binary Tree Level Order Traversal (Easy)

---
+++ Easy/102 Binary Tree Level Order Traversal.java
@@ -0,0 +1,70 @@
+/*
+This question is from https://leetcode.com/problems/binary-tree-level-order-traversal/
+Difficulty: easy
+
+Given a binary tree, return the level order traversal of its nodes' values. 
+(ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, 
+where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+    /
+   4
+    \
+     5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+
+
+Time Complexity: O(N) , N: the elements in the tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while(!q.isEmpty()){
+        	List<Integer> level = new LinkedList<Integer>();
+        	int length = q.size();
+        	for(int i = 0; i < length; i++){
+        		TreeNode node = q.poll();
+        		level.add(node.val);
+        		if(node.left != null) q.add(node.left);
+        		if(node.right != null) q.add(node.right);
+        	} 
+        	result.add(level);
+        } 
+        return result;
+    }
+}
