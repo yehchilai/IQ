4824ce72ad3a362c9bbb18c1b1027d4f627054d7
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-02-13 12:19:11 -0800

    88. Merge Sorted Array (Easy)

--- Easy/88 Merge Sorted Array.java
+++ Easy/88 Merge Sorted Array.java
@@ -12,6 +12,22 @@ Time complexity: O()
 */
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
+        while(n > 0){
+            if(m > 0 && nums1[m - 1] > nums2[n - 1]){
+                nums1[m + n - 1] = nums1[m - 1];
+                m--;
+            }else{
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
+            }
+        }
     }
+}
+
+// Alternative answer : https://leetcode.com/discuss/71809/my-2-lines-0ms-java-solution
+public class Solution{
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+    for (int i = m + n - 1; i >= 0; i--)
+        nums1[i] = m != 0 && (n == 0 || nums1[m - 1] > nums2[n - 1]) ? nums1[--m] : nums2[--n];
+}
 }
