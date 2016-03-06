5f66d5ad6cb9b27d461f660dd0d5b7d446d22f3f
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-03-05 22:31:57 -0800

    204. Count Primes - incomplete

---
+++ Easy/204 Count Primes.java
@@ -0,0 +1,16 @@
+/*
+Thiq question is from https://leetcode.com/problems/count-primes/
+Difficulty: easy
+
+
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Time Complexity: O()
+*/
+public class Solution {
+    public int countPrimes(int n) {
+        
+    }
+}
