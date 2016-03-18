40e80102d65f9fd9ccf7b026a81bc61c96732a5d
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-03-18 15:28:30 -0700

    278. First Bad Version

--- Easy/278 First Bad Version.java
+++ Easy/278 First Bad Version.java
@@ -13,7 +13,7 @@ Implement a function to find the first bad version.
 You should minimize the number of calls to the API.
 
 
-Time Complexity:()
+Time Complexity:(logN)
 */
 
 /* The isBadVersion API is defined in the parent class VersionControl.
@@ -21,6 +21,17 @@ Time Complexity:()
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+        int start = 1;
+        int end = n;
+
+        while(end > start){
+        	int mid = start + (end - start)/2;
+        	if(isBadVersion(mid)){
+        		end = mid;
+        	}else{
+        		start = mid + 1;
+        	}
+        }
+        return start;
     }
 }
