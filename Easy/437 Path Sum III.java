75d8263d3573a8cd735316b0e7e88b598a5cb123
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-11-01 15:39:24 -0700

    437. Path Sum III (easy)

---
+++ Easy/437 Path Sum III.java
@@ -0,0 +1,59 @@
+/*
+This question is from https://leetcode.com/problems/path-sum-iii/
+Difficulty: easy
+
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+
+*/
+
+// recursive, T:O(NlogN), M:O(N), 30ms
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        return sum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+
+    public int sum(TreeNode root, int sum){
+        int result = 0;
+        if(root == null) return 0;
+        if(root.val == sum) result++;
+        result = result + sum(root.left, sum - root.val);
+        result = result + sum(root.right, sum - root.val);
+        return result;
+    }
+}
+
+
