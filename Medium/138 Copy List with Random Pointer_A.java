3eb4b65e21e7dc89b82788e1e4bd563adc1cdb62
Yeh-chi Lai <yehchilai@csupomona.edu>
2018-05-21 11:45:01 -0700

    138.	Copy List with Random Pointer (medium)

---
+++ Medium/138 Copy List with Random Pointer_A.java
@@ -0,0 +1,94 @@
+/*
+This question is from https://leetcode.com/problems/copy-list-with-random-pointer/description/
+Difficulty: medium
+
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+
+
+*/
+// T: O(N), S: O(N), 5ms
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null) return null;
+
+        // current node
+        RandomListNode node = head;
+        // hashmap to record random point
+        HashMap<RandomListNode, RandomListNode> map = new HashMap();
+
+        // go through the original linked list to get all new node
+        while(node != null){
+            map.put(node, new RandomListNode(node.label));
+            node = node.next;
+        }
+
+        // connect random point and next point in the new linked list
+        node = head;
+        while(node != null){
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
+    }
+}
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43491/A-solution-with-constant-space-complexity-O(1)-and-linear-time-complexity-O(N)
+// T: O(N), S: O(1)
+public RandomListNode copyRandomList(RandomListNode head) {
+	RandomListNode iter = head, next;
+
+	// First round: make copy of each node,
+	// and link them together side-by-side in a single list.
+	while (iter != null) {
+		next = iter.next;
+
+		RandomListNode copy = new RandomListNode(iter.label);
+		iter.next = copy;
+		copy.next = next;
+
+		iter = next;
+	}
+
+	// Second round: assign random pointers for the copy nodes.
+	iter = head;
+	while (iter != null) {
+		if (iter.random != null) {
+			iter.next.random = iter.random.next;
+		}
+		iter = iter.next.next;
+	}
+
+	// Third round: restore the original list, and extract the copy list.
+	iter = head;
+	RandomListNode pseudoHead = new RandomListNode(0);
+	RandomListNode copy, copyIter = pseudoHead;
+
+	while (iter != null) {
+		next = iter.next.next;
+
+		// extract the copy
+		copy = iter.next;
+		copyIter.next = copy;
+		copyIter = copy;
+
+		// restore the original list
+		iter.next = next;
+
+		iter = next;
+	}
+
+	return pseudoHead.next;
+}
