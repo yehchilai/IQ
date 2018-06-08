94f794c272522359e6570f71f604f25c7ceeba88
Yeh-chi Lai <yehchilai@csupomona.edu>
2018-06-07 21:54:07 -0700

    538. Convert BST to Greater Tree.

---
+++ Easy/538 Convert BST to Greater Tree_A.java
@@ -0,0 +1,103 @@
+/*
+This question is from https://leetcode.com/problems/convert-bst-to-greater-tree/description/
+Difficulty: easy
+
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// T:O(N), S:O(N) becasue of recursive, 16ms
+class Solution {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        TreeNode node = root;
+        traversal(node);
+
+        return root;
+    }
+
+    private void traversal(TreeNode root){
+        if(root == null) return;
+        traversal(root.right);
+        root.val = root.val + sum;
+        sum = root.val;
+        traversal(root.left);
+    }
+}
+// T:O(N), S:O(1)
+class Solution {
+    /* Get the node with the smallest value greater than this one. */
+    private TreeNode getSuccessor(TreeNode node) {
+        TreeNode succ = node.right;
+        while (succ.left != null && succ.left != node) {
+            succ = succ.left;
+        }
+        return succ;
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+
+        while (node != null) {
+            /*
+             * If there is no right subtree, then we can visit this node and
+             * continue traversing left.
+             */
+            if (node.right == null) {
+                sum += node.val;
+                node.val = sum;
+                node = node.left;
+            }
+            /*
+             * If there is a right subtree, then there is at least one node that
+             * has a greater value than the current one. therefore, we must
+             * traverse that subtree first.
+             */
+            else {
+                TreeNode succ = getSuccessor(node);
+                /*
+                 * If the left subtree is null, then we have never been here before.
+                 */
+                if (succ.left == null) {
+                    succ.left = node;
+                    node = node.right;
+                }
+                /*
+                 * If there is a left subtree, it is a link that we created on a
+                 * previous pass, so we should unlink it and visit this node.
+                 */
+                else {
+                    succ.left = null;
+                    sum += node.val;
+                    node.val = sum;
+                    node = node.left;
+                }
+            }
+        }
+
+        return root;
+    }
+}
